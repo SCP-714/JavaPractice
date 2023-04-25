baa2da9b9af979aeb19675974f5641534bea39ac
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-25 13:57:48 -0500

    working with data typea

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        System.out.print("helloWorld");
+        //Java helloWorld
+        int x = 100;
+        double y = 3.141234567892345678;
+        System.out.println("helloWorld");
+        System.out.println("hello again");
+        System.out.print(x);
+        System.out.print(y);
     }
 }
