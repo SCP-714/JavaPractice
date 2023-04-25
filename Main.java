22fbd0780e855df8876a5f0e8f7f01259e83caed
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-25 16:28:18 -0500

    accepting multiple user inputs

--- Main.java
+++ Main.java
@@ -5,9 +5,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello User what is your name?");
-
         String name = scanner.nextLine();
 
+        System.out.println("How many cycles have you been in this world");
+        int cycles = scanner.nextInt();
+
         System.out.println("Ah I see, Welcome Home " + name);
+        System.out.println("You have been in the system for " + cycles + " cycles you must be the creator!");
+
     }
 }
