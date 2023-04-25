3f1521f7ddd550dc1c191e145bb74a3ec5bcc79d
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-25 16:07:53 -0500

    turned water into kool-aid

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args){
         //Java helloWorld
-        int x = 100;
-        double y = 3.141234567892345678;
-        System.out.println("helloWorld");
-        System.out.println("hello again");
-        System.out.print(x);
-        System.out.print(y);
+       String x = "Water";
+       String y = "Kool-aid";
+
+        x = y;
+        System.out.println(x);
     }
 }
